babd2d3b0d6d15a71a45e9ec2882f3a555d674b5
Richard Watts <richard@zilliqa.com>
2024-01-22 14:46:25 +0000

    Copy verifier code into run.go so that we can edit it (horrid, but the dependency issues are otherwise horrid)

--- cmd/tools/run.go
+++ cmd/tools/run.go
@@ -23,6 +23,7 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -39,9 +40,9 @@ import (
 	"github.com/Zilliqa/gozilliqa-sdk/account"
 	"github.com/Zilliqa/gozilliqa-sdk/core"
 	"github.com/Zilliqa/gozilliqa-sdk/crosschain/polynetwork"
+	"github.com/Zilliqa/gozilliqa-sdk/multisig"
 	"github.com/Zilliqa/gozilliqa-sdk/provider"
 	zilutil "github.com/Zilliqa/gozilliqa-sdk/util"
-	"github.com/Zilliqa/gozilliqa-sdk/verifier"
 
 	"github.com/btcsuite/btcd/wire"
 	types3 "github.com/cosmos/cosmos-sdk/types"
@@ -88,6 +89,10 @@ import (
 	"github.com/tendermint/tendermint/rpc/client/http"
 	types2 "github.com/tendermint/tendermint/types"
 )
+
+type Verifier struct {
+	NumOfDsGuard int
+}
 
 var (
 	tool                                                                  string
@@ -998,7 +1003,7 @@ func ZilReconstructGenesisHeader(poly *poly_go_sdk.PolySdk, inFile string, ouFil
 
 	log.Infof("Starting at txBlock %d, dsBlock %d and moving to TxBlock %d",
 		curTxBlockNum, curDsBlockNum, targetTxBlockNum)
-	verifier := &verifier.Verifier{
+	verifier := &Verifier{
 		NumOfDsGuard: numGuards,
 	}
 
@@ -1029,7 +1034,7 @@ func ZilReconstructGenesisHeader(poly *poly_go_sdk.PolySdk, inFile string, ouFil
 			nextDsBlockDecoded := core.NewDsBlockFromDsBlockT(nextDsBlock)
 			log.Infof("There are %d to be removed, and %d to be added",
 				len(nextDsBlockDecoded.BlockHeader.RemoveDSNodePubKeys), len(nextDsBlockDecoded.BlockHeader.PoWDSWinners))
-			newDsList, err2 := verifier.VerifyDsBlock(nextDsBlockDecoded, dsComm)
+			newDsList, err2 := verifier.VerifyDsBlock(nextDsBlock, nextDsBlockDecoded, dsComm)
 			if err2 != nil {
 				panic(fmt.Errorf("Cannot advance DS block past %d - %v", nextDsBlock, err2))
 			}
@@ -3287,4 +3292,186 @@ func CosmosDelegateToVal(amt int64) {
 	}
 	log.Infof("CosmosDelegateToVal, delegate %d %s to validator %s: txhash %s",
 		amt, demon, types3.ValAddress(invoker.Acc.Acc).String(), res.Hash.String())
+}
+
+func (v *Verifier) AggregatedPubKeyFromDsComm(dsComm *list.List, dsBlock *core.DsBlock) ([]byte, error) {
+	pubKeys, err := v.generateDsCommArray(dsComm, dsBlock)
+	if err != nil {
+		return nil, err
+	}
+	aggregatedPubKey, err := multisig.AggregatedPubKey(pubKeys)
+	if err != nil {
+		return nil, err
+	}
+	return aggregatedPubKey, nil
+}
+
+func (v *Verifier) AggregatedPubKeyFromTxComm(dsComm *list.List, txBlock *core.TxBlock) ([]byte, error) {
+	pubKeys, err := v.generateDsCommArray2(dsComm, txBlock)
+	if err != nil {
+		return nil, err
+	}
+	aggregatedPubKey, err := multisig.AggregatedPubKey(pubKeys)
+	if err != nil {
+		return nil, err
+	}
+	return aggregatedPubKey, nil
+}
+
+// abstract this two methods
+func (v *Verifier) generateDsCommArray(dsComm *list.List, dsBlock *core.DsBlock) ([][]byte, error) {
+	if dsComm.Len() != len(dsBlock.Cosigs.B2) {
+		return nil, errors.New("ds list mismatch")
+	}
+	bitmap := dsBlock.Cosigs.B2
+	quorum := len(bitmap) / 3 * 2
+	trueCount := 0
+	for _, signed := range bitmap {
+		if signed {
+			trueCount++
+		}
+	}
+	if !(trueCount > quorum) {
+		return nil, errors.New("quorum error")
+	}
+	var commKeys []string
+	cursor := dsComm.Front()
+	for cursor != nil {
+		pair := cursor.Value.(core.PairOfNode)
+		cursor = cursor.Next()
+		commKeys = append(commKeys, pair.PubKey)
+	}
+
+	var pubKeys [][]byte
+	for index, key := range commKeys {
+		if bitmap[index] {
+			pubKeys = append(pubKeys, zilutil.DecodeHex(key))
+		}
+	}
+	return pubKeys, nil
+}
+
+func (v *Verifier) generateDsCommArray2(dsComm *list.List, txBlock *core.TxBlock) ([][]byte, error) {
+	if dsComm.Len() != len(txBlock.Cosigs.B2) {
+		return nil, errors.New("ds list mismatch")
+	}
+	bitmap := txBlock.Cosigs.B2
+	quorum := len(bitmap) / 3 * 2
+	trueCount := 0
+	for _, signed := range bitmap {
+		if signed {
+			trueCount++
+		}
+	}
+	if !(trueCount > quorum) {
+		return nil, errors.New("quorum error")
+	}
+	var commKeys []string
+	cursor := dsComm.Front()
+	for cursor != nil {
+		pair := cursor.Value.(core.PairOfNode)
+		cursor = cursor.Next()
+		commKeys = append(commKeys, pair.PubKey)
+	}
+
+	var pubKeys [][]byte
+	for index, key := range commKeys {
+		if txBlock.Cosigs.B2[index] {
+			pubKeys = append(pubKeys, zilutil.DecodeHex(key))
+		}
+	}
+	return pubKeys, nil
+}
+
+// 0. verify current ds block
+// 2. generate next ds committee
+// return new ds comm
+func (v *Verifier) VerifyDsBlock(origDsBlock *core.DsBlockT, dsBlock *core.DsBlock, dsComm *list.List) (*list.List, error) {
+	newDsComm, err2 := v.UpdateDSCommitteeComposition("", dsComm, origDsBlock, dsBlock)
+	if err2 != nil {
+		return nil, err2
+	}
+	return newDsComm, nil
+}
+
+func (v *Verifier) VerifyTxBlock(txBlock *core.TxBlock, dsComm *list.List) error {
+	aggregatedPubKey, err := v.AggregatedPubKeyFromTxComm(dsComm, txBlock)
+	if err != nil {
+		return err
+	}
+	r, s := txBlock.GetRandS()
+	if !multisig.MultiVerify(aggregatedPubKey, txBlock.Serialize(), r, s) {
+		msg := fmt.Sprintf("verify tx block %d error", txBlock.BlockHeader.BlockNum)
+		return errors.New(msg)
+	}
+	return nil
+}
+
+func (v *Verifier) UpdateDSCommitteeComposition(selfKeyPub string, dsComm *list.List, origDsBlock *core.DsBlockT, dsBlock *core.DsBlock) (*list.List, error) {
+	var dummy core.MinerInfoDSComm
+	return v.updateDSCommitteeComposition(selfKeyPub, dsComm, origDsBlock, dsBlock, dummy)
+}
+
+// inner type of dsComm is core.PairOfNode
+func (v *Verifier) updateDSCommitteeComposition(selfKeyPub string, dsComm *list.List, origDsBlock *core.DsBlockT,
+	dsBlock *core.DsBlock, info core.MinerInfoDSComm) (*list.List, error) {
+	// 0. verify ds block first
+	aggregatedPubKey, err := v.AggregatedPubKeyFromDsComm(dsComm, dsBlock)
+	if err != nil {
+		return nil, err
+	}
+	headerBytes := dsBlock.Serialize()
+	r, s := dsBlock.GetRandS()
+
+	if !multisig.MultiVerify(aggregatedPubKey, headerBytes, r, s) {
+		msg := fmt.Sprintf("verify ds block %d error", dsBlock.BlockHeader.BlockNum)
+		return nil, errors.New(msg)
+	}
+
+	// 1. get the map of all pow winners from the DS block
+	winners := dsBlock.BlockHeader.PoWDSWinners
+	numOfWinners := len(dsBlock.BlockHeader.PoWDSWinners)
+
+	// 2. get the array of all non-performant nodes to be removed
+	removeDSNodePubkeys := dsBlock.BlockHeader.RemoveDSNodePubKeys
+
+	// 3. shuffle the non-performant nodes to the back
+	for _, removed := range removeDSNodePubkeys {
+		current := dsComm.Front()
+		for current != nil {
+			pairOfNode := current.Value.(core.PairOfNode)
+			if pairOfNode.PubKey == removed {
+				break
+			}
+			current = current.Next()
+		}
+		if current != nil {
+			dsComm.MoveToBack(current)
+		}
+	}
+
+	// 4. add new winners
+	for _, pubKey := range origDsBlock.Header.PoWWinners {
+		peer := winners[pubKey]
+		w := core.PairOfNode{
+			PubKey: pubKey,
+			Peer:   peer,
+		}
+		// Place the current winner node's information in front of the DS Committee
+		count := v.NumOfDsGuard
+		cursor := dsComm.Front()
+		for count > 0 {
+			count--
+			cursor = cursor.Next()
+		}
+		dsComm.InsertBefore(w, cursor)
+	}
+
+	// 5. remove one node for every winner, maintaining the size of the DS Committee
+	for i := 0; i < numOfWinners; i++ {
+		back := dsComm.Back()
+		dsComm.Remove(back)
+	}
+
+	return dsComm, nil
 }
